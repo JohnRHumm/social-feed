ae6ddfaea63dd85a7c0ef148572e40fa591a5694
Humm <jhumm@mcw.edu>
2022-07-02 09:50:54 -0500

    feat(jsx) Added like and dislike buttons to PostFeed

--- src/Components/PostFeed/PostFeed.jsx
+++ src/Components/PostFeed/PostFeed.jsx
@@ -1,4 +1,7 @@
 
+import likeImage from "./thumbsUp.png";
+import dislikeImage from "./thumbsDown.png";
+
 const PostFeed = ({posts}) => {
     
   
@@ -7,12 +10,12 @@ const PostFeed = ({posts}) => {
             {posts.map((post,index) => {
                if (post.id >0) {
                 return (
-                    <div key={post.id}> 
+                    <div key={post.id} className = 'postContainer'> 
                         <p id='name'>{post.name}</p>
                         <p id='post'>{post.post}</p>
                         <p id='date'>{post.date}</p>
-                        <p id='like'>Like: {post.like}</p>
-                        <p id='dislike'>Dislike: {post.dislike}</p>
+                        <button type='button' name='like' id='like'><img src={likeImage}/></button>
+                        <button type='button' name='dislike' id='dislike'><img src={dislikeImage}/></button>
                     </div>
                 );}
             })}
